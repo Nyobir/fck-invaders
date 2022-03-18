c10965a9c6c4d5189960d49183c86f9c75771582
Nyobir <nyobir@gmail.com>
2022-03-18 23:28:36 +0200

    Bmon installer

--- attacker-setup.sh
+++ attacker-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt update;
-apt install git python3 python3-pip -y;
+apt install git python3 python3-pip bmon -y;
 
 git clone https://github.com/MHProDev/MHDDoS.git;
 pip3 install -r ./MHDDoS/requirements.txt;
