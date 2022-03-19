be9034f7de73c6309249ed530e5698da470c4151
Nyobir <nyobir@gmail.com>
2022-03-19 22:43:32 +0200

    Script autoupdate

--- botnet.sh
+++ botnet.sh
@@ -7,7 +7,7 @@ SCRIPTNAME="$0"
 cd $SCRIPTPATH
 git fetch
 
-[[ -n $(git diff --name-only origin/master) ]] && {
+[[ -n $(git log ..origin/master) ]] && {
     echo "Found a new version of me, updating myself..."
     git checkout master
     git pull --force
