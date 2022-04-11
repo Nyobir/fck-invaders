cc786d70134d916af1b3cf2e5080a884c434ef1f
Nyobir <nyobir@gmail.com>
2022-04-11 21:21:59 +0300

    Dynamic proxy file name

--- menus/proxy_file.sh
+++ menus/proxy_file.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 MENU_OPTION=$(dialog --stdout --title "Proxy file " \
---inputbox "Enter proxy list filename:" 8 50 'proxy.txt')
+--inputbox "Enter proxy list filename:" 8 50 "proxy-$(date +'%d-%m').txt")
 
 echo $MENU_OPTION
 exit
