3e686adecf984989afd3b8b0a0166c8e5e7ebd42
Nyobir <nyobir@gmail.com>
2022-04-16 13:08:41 +0300

    MacOS perl grep fix

--- menus/it_ukraine_targets.sh
+++ menus/it_ukraine_targets.sh
@@ -3,9 +3,9 @@
 targets=$(dialog --stdout --title "Target " \
 --inputbox "Paste targets from https://itarmy.com.ua/check/:" 8 50)
 
-protocol_counts=$(grep -Po "[a-z]+" <<< $targets | uniq -c)
+protocol_counts=$(grep -Eo "[a-z]+" <<< "$targets" | uniq -c)
 chosen_protocol=$(dialog --title 'Choose prefered targets' --menu --stdout "Targets by protocol count:" 15 25 "$($protocol_counts | wc -l)" $protocol_counts)
 chosen_protocol=$(grep "$chosen_protocol" <<< "$protocol_counts" | awk '{print $2}')
 
-grep -Po  "$chosen_protocol:\/\/(\d+\.\d+\.\d+\.\d+:\d+)" <<< "$targets" | grep -Po  "\d+\.\d+\.\d+\.\d+:\d+" > "$SCRIPT_DIR/targets.txt"
+grep -Eo "$chosen_protocol:\/\/([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+:[0-9]+)" <<< "$targets" | grep -Eo "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+:[0-9]+" > "$SCRIPT_DIR/targets.txt"
 exit
