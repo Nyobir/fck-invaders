a65124e53b122a712a199e4c5eabe1d40c4cd38b
Nyobir <nyobir@gmail.com>
2022-04-28 20:05:14 +0300

    It army parser: http protocol

--- menus/it_ukraine_targets.sh
+++ menus/it_ukraine_targets.sh
@@ -4,8 +4,13 @@ targets=$(dialog --stdout --title "Target " \
 --inputbox "Paste targets from https://itarmy.com.ua/check/:" 8 50)
 
 protocol_counts=$(grep -Eo "[a-z]+://" <<< "$targets" | grep -Eo "[a-z]+" | uniq -c)
-chosen_protocol=$(dialog --title 'Choose prefered targets' --menu --stdout "Targets by protocol count:" 15 25 "$($protocol_counts | wc -l)" $protocol_counts)
+chosen_protocol=$(dialog --title 'Choose preferred targets' --menu --stdout "Targets by protocol count:" 15 25 "$($protocol_counts | wc -l)" $protocol_counts)
 chosen_protocol=$(grep "$chosen_protocol" <<< "$protocol_counts" | awk '{print $2}')
 
-grep -Eo "$chosen_protocol:\/\/([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+:[0-9]+)" <<< "$targets" | grep -Eo "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+:[0-9]+" > "$SCRIPT_DIR/targets.txt"
+if grep -q 'http' <<< "$chosen_protocol"; then
+  grep -Eo "$chosen_protocol:\/\/([-a-zA-Z0-9\.\/\?\=\%\&\_]+)" <<< "$targets" > "$SCRIPT_DIR/targets.txt"
+else
+  grep -Eo "$chosen_protocol:\/\/([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+:[0-9]+)" <<< "$targets" | grep -Eo "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+:[0-9]+" > "$SCRIPT_DIR/targets.txt"
+fi
+
 exit
